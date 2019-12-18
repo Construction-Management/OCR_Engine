98c0fc76865f1bba75a97a3e806ef2208b17fcee
Mohit Gupta <mohitgupta2402@gmail.com>
2019-12-18 11:56:39 +0530

    total amount updated

--- extraction.py
+++ extraction.py
@@ -15,9 +15,11 @@ print(output_dict)
 #regex for dates
 date_pattern='^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-(19|20)\d\d$'
 order_id_pattern='(OD\d+)'
+total_amount_pattern='(\d+[.]\d\d)'
 #building boxes around the text having >60% of confidence.
 #follows the pattern
 l=[]
+d=[]
 boxes=len(output_dict['text'])
 for i in range(boxes):
     if int(output_dict['conf'][i])>60:
@@ -25,6 +27,10 @@ for i in range(boxes):
             x,y,w,h = output_dict['left'][i],output_dict['top'][i],output_dict['width'][i],output_dict['height'][i]
             img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
             l.append(output_dict['text'][i])
+        if re.match(total_amount_pattern,output_dict['text'][i]):
+            x,y,w,h = output_dict['left'][i],output_dict['top'][i],output_dict['width'][i],output_dict['height'][i]
+            img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            d.append(output_dict['text'][i])
         #else:
            # print('not')
 cv2.imshow('img',img)
@@ -32,4 +38,6 @@ cv2.waitKey(0)
 print("Order ID: "+l[0])
 print("Order Date: "+l[1])
 print("Invoice Date: "+l[2])
+print("Total Amount: "+d[-1])
 print(l)
+print(d)
